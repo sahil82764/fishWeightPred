8c137beacd8e7ea9d9b4a645ce5d91306f8fff46
sahil82764 <sahil82764@gmail.com>
2024-03-18 19:25:21 -0400

    files added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+import joblib
 
 app = Flask(__name__)
 
@@ -19,6 +20,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 # Create and train the model
 model = RandomForestRegressor(n_estimators=100)
 model.fit(X_train, y_train)
+
+# Save the model to a file
+joblib.dump(model, 'fishWeight.pkl')
 
 @app.route('/')
 def index():
